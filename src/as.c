bc3da603bddc6d7f009311a7452af7292fa084ff
roland <roland@van-straten.org>
2026-02-24 15:14:12 +0100

    Use ";" also as a comment delimiter alongside the "*"

--- src/as.c
+++ src/as.c
@@ -187,7 +187,7 @@ int parse_line(void)
 	register char *ptrto = Label;
 	char	*skip_white(char *);
 
-	if( *ptrfrm == '*' || *ptrfrm == '\n' )
+	if( *ptrfrm == ';' || *ptrfrm == '*' || *ptrfrm == '\n' )
 		return(0);	/* a comment line */
 
 	while( delim(*ptrfrm)== NO )
